c3158a365ec5954961289f40baf7bf63239ae5a5
Alex Ackerman <105178749+ackerman-alex@users.noreply.github.com>
2024-10-24 16:58:40 +0000

    rewrote main python file

--- main.py
+++ main.py
@@ -1,37 +1,18 @@
 '''
-    Python Library
+    Main Python File
 '''
 
-import pandas as pd
-import time
-import resource
+from library.average import average, calculate_time_memory
 
-def average(path):
-    birth_data = pd.read_csv(path)
+data_path = "births14.csv"
 
-    weight_avg = birth_data["weight"].mean()
-    return weight_avg
+if __name__ == "__main__":
+    data_path = "births14.csv"  # Updated with the actual CSV file path
 
-def calculate_time_memory(path):
-    # Record the start time
-    start_time = time.time()
+    avg_weight = average(data_path)
+    print(f"Average weight: {avg_weight:.5}")
 
-    # Measure the initial resource usage
-    start_mem_usage = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+    end_mem_usage, elapsed_time = calculate_time_memory(data_path)
 
-    # Calculate the average
-    average(path)
-
-    # Record the end time
-    end_time = time.time()
-
-    # Calculate the elapsed time
-    elapsed_time = end_time - start_time
-
-    # Measure the final resource usage
-    end_mem_usage = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-
-    print(f"Python-Elapsed Time: {elapsed_time:.7f} seconds")
-    print(f"Python-Memory Usage Change: {end_mem_usage - start_mem_usage} kilobytes")
-
-    return end_mem_usage, elapsed_time
+    print(f"Final Memory Usage: {end_mem_usage} kilobytes")
+    print(f"Total Elapsed Time: {elapsed_time:.7} seconds")
